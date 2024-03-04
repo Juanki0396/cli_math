556475d7b9f8c70a5f012a0aecaf5b2ab7b0e456
Juan Carlos Aranda Huecas <juanki0396@gmail.com>
2024-03-04 20:41:34 +0100

    Update main to run basic example

--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,12 @@
-let example = "1 - 2 * 34 + (8-1)"
+open Cli_math
 
-type operator = 
-        | Addition
-        | Substraction
-        | Multiplication
-        | Division
+let example = "6 + 4 / 2"
 
-type token =
-        | Number of float
-        | Parenthesis_l
-        | Parenthesis_r
-        | Operator of operator
-        | Identificator of string
+let ast =
+        match Lexer.scan_token example with
+        | Ok tok_lst -> Parser.parse (Parser.new_parser tok_lst)
+        | Error _ -> raise (Invalid_argument "Invalid input")
 
-let extract_fst_num str =
-        let rec aux acc s =
-                let next = String.get s 0 in
-                let len = String.length str in
-                let substr = String.sub str 1 (len - 1) in
-                match next with
-                | '0'..'9' -> aux (acc ^ String.make 1 next) substr
-                | '.' when Bool.not (String.contains acc '.') -> aux (acc ^ String.make 1 next) substr
-                | _ -> (substr, Float.of_string (acc ^ String.make 1 next))
-        in
-        aux "" str
+let n = Eval.evaluate ast
 
-let () = print_float (snd (extract_fst_num example))
+let () = Printf.printf "%s = %f" example n
